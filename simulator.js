6042b96932c87a1f89be934f8e9425007c47e8de
DIRECT0R-ctrl <youcodestudent@proton.me>
2025-10-30 23:08:34 +0100

    js(getEls) : object

---
+++ simulator.js
@@ -0,0 +1,16 @@
+function getEls() {
+  return {
+    amountEl: document.getElementById('disabledTextInput'),
+    durationEl: document.getElementById('disabledTextInput2'),
+    durationUnit: document.getElementById('disabledSelect'),
+    calculationBtn: document.getElementById('btnCalculation'),
+    monthlyPaymentEl: document.getElementById('monthly-payment'),
+    warningEl: document.getElementById('myimg'),
+    salaryEl: document.getElementById('slaryInput'),
+  };
+}
+
+const els = getEls();
+//console.log(els);
+
+
