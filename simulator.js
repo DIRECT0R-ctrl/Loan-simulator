c9d3ad760c0d0b03dd4d04ae54343bb27ca88f92
DIRECT0R-ctrl <youcodestudent@proton.me>
2025-10-30 23:31:10 +0100

    js(style&logic): warning message

--- simulator.js
+++ simulator.js
@@ -14,3 +14,41 @@ const els = getEls();
 //console.log(els);
 
 
+els.calculationBtn.addEventListener('click', function() {
+  
+  const amount = els.amountEl.value;
+  const duration = els.durationEl.value;
+  const unit = els.durationUnit.value;
+
+  console.log(amount, duration, unit);
+
+  let durationInMonths;
+
+  if (unit === "Years") {
+    durationInMonths = Number(duration * 12);
+  } else if (unit === "months") {
+    durationInMonths = Number(duration);
+  } else if (unit === 'Days'){
+    durationInMonths = Number(duration / 30);
+  }
+
+
+  const monthlyPayment = Number(amount) / durationInMonths;
+
+ 
+  els.monthlyPaymentEl.textContent = "$" + monthlyPayment.toFixed(2);
+  // const text = node.textContent;
+
+  const salary = Number(els.salaryEl.value);
+
+
+
+  const ratio = (monthlyPayment / salary) * 100;
+
+  if (ratio > 40) 
+  {
+    els.warningEl.style.display = "flex";
+  } else {
+    els.warningEl.style.display = 'none';
+  }
+});
